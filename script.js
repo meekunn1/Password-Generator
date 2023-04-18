48cdf2968c352b1e8a4cead68e12ffc6fc9175ec
Keiji O <meekunn@gmail.com>
2023-04-17 23:46:01 -0700

    checked for bugs

--- script.js
+++ script.js
@@ -3,17 +3,16 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var poolBase = "abcdefghijklmnopqrstuvwxyz";
-  var poolAddNumber = "0123456789";
+  var poolBase = "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz";
+  var poolAddNumber = "01234567890123456789";
   var poolAddUppercase = poolBase.toUpperCase();
   var poolAddSpecialCharacter = "\ \!\"\#\$\%\/\&'\(\)\*\+\,\-\.\/\:\;\<\=\>\?\@\[\]\^\_\`\{\|\}\~";
   var scope = {
     characterLength: prompt("Lets begin the password creation process. \n Please input length of password. \n Please input a number between 8 and 128."),
-    cap: confirm("Include upper case?"),
-    number: confirm("Include numbers?"),
-    specialCharacter: confirm("Include special characters?"),
+    cap: confirm("Include Capital Letters?"),
+    number: confirm("Include Numbers?"),
+    specialCharacter: confirm("Include Special Characters?"),
   }
-  console.log(scope.characterLength)
   var characterConfirm = {
     cap: "Capital Letters ❌",
     number: "Numbers ❌",
@@ -48,7 +47,10 @@ function writePassword() {
     // do nothing
   }
   var settingCheck = confirm("Password will be " + scope.characterLength + " characters long and will have the following features:\n" + characterConfirm.cap + "\n" + characterConfirm.number + "\n" + characterConfirm.specialCharacter)
+  
+  //check random pull and length
   console.log(poolBase);
+  console.log(scope.characterLength);
 
   function generatePassword() {
   var result = "";
@@ -57,74 +59,19 @@ function writePassword() {
     result += poolBase.charAt(Math.floor(Math.random() * poolBase.length));
     counter += 1;
   }
+
+if (settingCheck === true){
   return result;
 }
-console.log(generatePassword());
-
+else 
+  alert("Please try again if you change your mind.");
+  return "Your Secure Password";
+}
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 }
 
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
-
-
-
-//test
-
-function makeid(length) {
-  let result = '';
-  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  const charactersLength = characters.length;
-  let counter = 0;
-  while (counter < length) {
-    result += characters.charAt(Math.floor(Math.random() * charactersLength));
-    counter += 1;
-  }
-  return result;
-}
-
-console.log(makeid(50));
-
-
-//loop test
-  // Creates an array containing names of five student in the class
-  var students = ["Sarah", "Orlando", "Heather", "Ismael", "Hung"];
-
-  // Logs length of the array
-  console.log(students.length);
-
-  // For loop starts at 0, runs while i is less than length of student array
-  // Increments by 1
-  for(var i=0; i < students.length; i++) {
-    // This statement will run each time the loop is executed
-    console.log("Great to see you, " + students[i] + "!");
-  }
-
-
-//test work by keiji
-
-  
-// var random = Math.floor(Math.random() * against.length);
-  
-//  console.log("player choose " + action)
-//  console.log("computer random generate " + random + " so computer choose " + against[random]);
-  
-//  var computer = against[random]
-
-// "\ \!\"\#\$\%\/\&\'\(\)\*\+\,\-\.\/\:\;\<\=\>\?\@\[\\\]\^\_\`\{\|\}\~"
-var test1 = "abcdefghijklmnopqrstuvwxyz0123456789";
-var test4 = "0123456789";
-var test5 = test1.toUpperCase();
-var test2 = "\ \!\"\#\$\%\/\&'\(\)\*\+\,\-\.\/\:\;\<\=\>\?\@\[\]\^\_\`\{\|\}\~";
-var test3 = test1.concat(test2, test4, test5);
-console.log(test1);
-console.log(test2);
-console.log(test4);
-console.log(test5);
-console.log(test3);
